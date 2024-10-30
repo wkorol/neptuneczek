c5d121e01ba20fed9abb5739d5f1bd56a4ec5506
Wiktor Korol <wiktor.korol@iteo.com>
2024-10-30 08:56:03 +0100

    fixes

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use App\Repository\PersonRepository;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PersonController extends AbstractController
 {
-    public function __construct(private readonly PersonRepository $repository)
+    public function __construct(private LoggerInterface $logger, private readonly PersonRepository $repository)
     {
 
     }
@@ -17,6 +18,7 @@ class PersonController extends AbstractController
     #[Route('api/persons', name: 'app_person')]
     public function index(): JsonResponse
     {
+        $this->logger->info('Test log message');
         $persons = $this->repository->findAll();
         return $this->json($persons);
     }
